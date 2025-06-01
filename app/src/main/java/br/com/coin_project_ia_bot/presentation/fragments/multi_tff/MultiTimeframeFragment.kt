9710722ab142147ae9595c5f509c3a8aad22c56f
enzotelesvia <enzo.teles@viavarejo.com.br>
2025-06-01 16:41:20 -0300

    implementando o navigation e o bottom navigation no projeto

--- app/src/main/java/br/com/coin_project_ia_bot/presentation/fragments/multi_tff/MultiTimeframeFragment.kt
+++ app/src/main/java/br/com/coin_project_ia_bot/presentation/fragments/multi_tff/MultiTimeframeFragment.kt
@@ -15,11 +15,11 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import br.com.coin_project_ia_bot.databinding.FragmentMultiTfBinding
+import br.com.coin_project_ia_bot.databinding.FragmentMultiTimeframeBinding
 
 class MultiTimeFragment : Fragment() {
 
-    private var _binding: FragmentMultiTfBinding? = null
+    private var _binding: FragmentMultiTimeframeBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var adapter: MultiTimeAdapter
@@ -30,7 +30,7 @@ class MultiTimeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMultiTfBinding.inflate(inflater, container, false)
+        _binding = FragmentMultiTimeframeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
